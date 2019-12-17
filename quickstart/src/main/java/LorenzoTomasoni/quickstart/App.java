2b4b70b3b1e75345bfe6b8baba53391d4cc46809
Lorenzo Tomasoni <l.tomasoni4@campus.unimib.it>
2019-12-17 12:13:47 +0100

    Esercizio 1 di maven

---
+++ quickstart/src/main/java/LorenzoTomasoni/quickstart/App.java
@@ -0,0 +1,11 @@
+package LorenzoTomasoni.quickstart;
+
+import com.google.gson.Gson;
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Gson gson=new Gson();
+        System.out.println( gson.toJson("Hello World!" ));
+    }
+}
